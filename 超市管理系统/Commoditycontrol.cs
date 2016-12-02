8cb6da816bf7a8e647c4b78bc782eda793bdacb5
unknown <>
2016-12-02 10:50:04 +0800

    start

---
+++ 超市管理系统/Commoditycontrol.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 超市管理系统 {
+    class CommodityControl {
+        public bool Stock(LogMessage) {
+            return true;
+        }
+
+        public CommodityMessage getCommodityMessage(string id) {
+
+        }
+
+        public bool sellCommodity(LogMessage) {
+
+        }
+
+        public List<CommodityMessage> getAllCommodityMessage() {
+
+        }
+
+        public bool modifyCommodity(CommodityMessage Mes) {
+
+        }
+
+        public bool clearRepo(int num) {
+
+        }
+
+    }
+}
---
+++ 超市管理系统/Struct&enum.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 超市管理系统 {
+    /// <summary>
+    /// 0:：插入成功
+    /// 1：编号重复
+    /// 2：服务器错误
+    /// </summary>
+    enum stockCode { 0,2,3, } ;
+
+    struct CommodityMessage {
+        string commodityName;
+        int num;
+        float inPrice;
+        float outPrice;
+        int id;
+    }
+
+    struct LogMessage {
+        bool mes;
+        string commodityName;
+        int num;
+        float price;    //打完折的价格
+        DateTime time;
+    }
+
+    struct Money {
+        float inMoney;
+        float outMoney;
+    }
+}
